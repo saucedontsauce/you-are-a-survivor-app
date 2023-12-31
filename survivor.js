1efd63357ef4b4c65db8e0e43f7f56ba6cae6ac2
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2023-12-31 20:27:20 +0000

    slight changes

--- survivor.js
+++ survivor.js
@@ -1,6 +1,6 @@
 
 function enterUserAge(){
-      
+    let disp = document.getElementById("indexResponse");
     let years = document.getElementById("userAge").value;
     let months = years * 12;
     let weeks = years * 52;
@@ -8,16 +8,12 @@ function enterUserAge(){
     let hours = days * 24;
     let minutes = hours * 60;
     let seconds = minutes * 60;   
-
-    document.getElementById("indexResponse").insertAdjacentHTML("afterend", "<p>" + seconds + " seconds<p>");
-    document.getElementById("indexResponse").insertAdjacentHTML("afterend", "<p>" + minutes + " minutes<p>");
-    document.getElementById("indexResponse").insertAdjacentHTML("afterend", "<p>" + hours + " hours<p>");
-    document.getElementById("indexResponse").insertAdjacentHTML("afterend", "<p>" + days + " days<p>");
-    document.getElementById("indexResponse").insertAdjacentHTML("afterend", "<p>" + weeks + " weeks<p>");
-    document.getElementById("indexResponse").insertAdjacentHTML("afterend", "<p>" + months + " months<p>");
-    document.getElementById("indexResponse").insertAdjacentHTML("afterend", "<p>" + years + " years<p>");
-}
-
-function enterUserAgeAgain(){
-    location.reload();
+    disp.innerHTML = ""
+    disp.insertAdjacentHTML("beforeend", "<p>" + seconds + " seconds<p>");
+    disp.insertAdjacentHTML("beforeend", "<p>" + minutes + " minutes<p>");
+    disp.insertAdjacentHTML("beforeend", "<p>" + hours + " hours<p>");
+    disp.insertAdjacentHTML("beforeend", "<p>" + days + " days<p>");
+    disp.insertAdjacentHTML("beforeend", "<p>" + weeks + " weeks<p>");
+    disp.insertAdjacentHTML("beforeend", "<p>" + months + " months<p>");
+    disp.insertAdjacentHTML("beforeend", "<p>" + years + " years<p>");
 }
